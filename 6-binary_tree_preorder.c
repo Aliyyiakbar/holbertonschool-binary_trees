89568a886a4ea4aa54756449e43f902c0551a538
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-29 15:26:55 +0400

    Add binary_tree_preorder function for preorder traversal of a binary tree

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ * @func: Pointer to a function to call for each node
+ *
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
