4ea6e49862d32f367924bc412a7e539f50135097
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-30 14:31:51 +0400

    Fix function name for sibling retrieval to maintain consistency

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - Finds the sibling of a node
+ * _binary_tree_sibling - Finds the sibling of a node
  * @node: Pointer to the node to find the sibling of
  *
  * Return: Pointer to the sibling node, or NULL if no sibling or node is NULL
  */
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *_binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 	{
@@ -40,5 +40,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 	}
 
-	return (binary_tree_sibling(node->parent));
+	return (_binary_tree_sibling(node->parent));
 }
