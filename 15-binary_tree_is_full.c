63ab479a2766ecb50b55ce9fc5e31db8887f8985
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-30 10:45:40 +0400

    Delete empty whitespaces at the end of include lines.

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,7 +5,7 @@
  * @tree: Pointer to the root node of the tree to check
  *
  * Return: 1 if the tree is full, 0 otherwise
- * A full binary tree is a tree where every 
+ * A full binary tree is a tree where every
  * node has either 0 or 2 children
  * An empty tree is considered full
  */
