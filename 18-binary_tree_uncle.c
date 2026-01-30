cadea482ca75d8d5419d6cd6c989b50f06615465
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-30 14:26:57 +0400

    Add function to find the uncle of a binary tree node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	return (binary_tree_sibling(node->parent));
+}
