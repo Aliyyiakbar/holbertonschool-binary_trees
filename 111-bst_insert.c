4e974f343d0992ae31f0042698a212566e6d6a05
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-31 23:41:28 +0400

    Refactor bst_insert function to improve clarity by extracting the parent finding logic into a separate function

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,47 +1,72 @@
 #include "binary_trees.h"
 
 /**
+ * findPar - find par where a new value should be inserted
+ * @root: root of the tree to search
+ * @value: value to insert
+ * @dup: out flag set to 1 if value already exists, else 0
+ *
+ * Return: pointer to par where new node should be attached,
+ *         or pointer to existing node if duplicate (and *dup == 1).
+ */
+static bst_t *findPar(bst_t *root, int value, int *dup)
+{
+	bst_t *cur = root, *par = NULL;
+
+	*dup = 0;
+
+	while (cur != NULL)
+	{
+		par = cur;
+
+		if (value == cur->n)
+		{
+			*dup = 1;
+			return (cur);
+		}
+
+		cur = (value < cur->n) ? cur->left : cur->right;
+	}
+
+	return (par);
+}
+
+/**
  * bst_insert - Inserts a value into a Binary Search Tree
  * @tree: Double pointer to the root node of the BST
  * @value: Value to insert into the BST
  *
  * Return: Pointer to the newly inserted node, or NULL on failure / duplicate
  */
-
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *cur, *par = NULL, *new;
+	bst_t *par, *new;
+	int dup;
 
 	if (tree == NULL)
 	{
 		return (NULL);
 	}
-	cur = *tree;
-	while (cur != NULL)
+
+	par = findPar(*tree, value, &dup);
+
+	if (dup)
 	{
-		par = cur;
-		if (value == cur->n)
-		{
-			return (NULL);
-		}
-		else if (value < cur->n)
-		{
-			cur = cur->left;
-		}
-		else
-		{
-			cur = cur->right;
-		}
+		return (NULL);
 	}
+
 	new = malloc(sizeof(bst_t));
+
 	if (new == NULL)
 	{
 		return (NULL);
 	}
+
 	new->n = value;
 	new->parent = par;
 	new->left = NULL;
 	new->right = NULL;
+
 	if (par == NULL)
 	{
 		*tree = new;
