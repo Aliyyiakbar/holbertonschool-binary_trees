02cad25e47c83c94ffa2e37a391e6e210687b31e
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-31 23:46:49 +0400

    Refactor array_to_bst function documentation and improve loop increment style

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree);
+
+/**
+ * bst_insert - Inserts a value into a Binary Search Tree
+ * @tree: Double pointer to the root node of the BST
+ * @value: Value to insert into the BST
+ *
+ * Return: Pointer to the newly inserted node, or NULL on failure / duplicate
+ */
+bst_t *bst_insert(bst_t **tree, int value);
+
+/**
  * array_to_bst - Builds a Binary Search Tree from an array
  * @array: Pointer to the first element of the array to be converted
  * @size: Number of elements in the array
@@ -18,7 +36,7 @@ bst_t *array_to_bst(int *array, size_t size)
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; ++i)
 	{
 		if (bst_insert(&root, array[i]) == NULL)
 		{
