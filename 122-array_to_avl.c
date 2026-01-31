0daf929c18eeed01997e1d69fdd3bcfc8407e94c
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-02-01 02:18:10 +0400

    Refactor array_to_avl function to improve structure and add avl_search for value lookup

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,28 +1,65 @@
 #include "binary_trees.h"
 
 /**
+ * avl_search - Searches for a value in an AVL tree
+ * @tree: Pointer to the root node
+ * @value: Value to search for
+ *
+ * Return: Pointer to the node if found, otherwise NULL
+ */
+static avl_t *avl_search(avl_t *tree, int value)
+{
+	while (tree != NULL)
+	{
+		if (value == tree->n)
+		{
+			return (tree);
+		}
+		if (value < tree->n)
+		{
+			tree = tree->left;
+		}
+		else
+		{
+			tree = tree->right;
+		}
+	}
+	return (NULL);
+}
+
+/**
  * array_to_avl - Builds an AVL tree from an array
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  *
- * Return: Pointer to the root node of the created AVL tree,
- *         or NULL on failure
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
-
 avl_t *array_to_avl(int *array, size_t size)
 {
-	avl_t *tree = NULL;
+	avl_t *tree, *node;
 	size_t i;
 
 	if (array == NULL || size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
-	for (i = 0; i < size; ++i)
+	tree = NULL;
+
+	for (i = 0; i < size; i++)
 	{
-		tree = avl_insert(&tree, array[i]);
+		if (avl_search(tree, array[i]) != NULL)
+		{
+			continue;
+		}
+
+		node = avl_insert(&tree, array[i]);
+		if (node == NULL)
+		{
+			binary_tree_delete(tree);
+			return (NULL);
+		}
 	}
 
-	return tree;
+	return (tree);
 }
