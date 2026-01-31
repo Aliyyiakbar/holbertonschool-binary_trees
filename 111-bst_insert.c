4e682ed1d871a5ec605026d5b3b21ab70e3afbfd
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-31 20:37:25 +0400

    Refactor bt_node function to improve parameter naming and enhance clarity in bst_insert function

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -4,13 +4,13 @@
 /**
  * bt_node - Creates a binary tree node
  *
- * @parent: Pointer to the parent node
+ * @par: Pointer to the par node
  * @value: Value to put in the new node
  *
  * Return: Pointer to the new node, or NULL on failure
  */
 
-binary_tree_t *bt_node(binary_tree_t *parent, int value)
+binary_tree_t *bt_node(binary_tree_t *par, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
@@ -20,68 +20,69 @@ binary_tree_t *bt_node(binary_tree_t *parent, int value)
 	}
 
 	new_node->n = value;
-	new_node->parent = parent;
+	new_node->parent = par;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
 	return (new_node);
 }
 
-/**
- * bst_insert - Inserts a value into a Binary Search Tree
- *
- * @tree: Double pointer to the root node of the BST
- * @value: Value to be inserted
- *
- * Return: Pointer to the newly inserted node, or NULL on failure
- */
-
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *nw, *cur, *par;
+	bst_t *new, *cur, *par;
 
 	if (tree == NULL)
 	{
 		return (NULL);
 	}
-	nw = bt_node(NULL, value);
-	if (nw == NULL)
+
+	new = (bst_t *)bt_node(NULL, value);
+
+	if (new == NULL)
 	{
 		return (NULL);
 	}
+
 	if (*tree == NULL)
 	{
-		*tree = nw;
-		return (nw);
+		*tree = new;
+
+		return (new);
 	}
+
 	cur = *tree;
-	while (1)
+	par = NULL;
+
+	while (cur != NULL)
 	{
 		par = cur;
+
 		if (value < cur->n)
 		{
 			cur = cur->left;
-			if (cur == NULL)
-			{
-				par->left = nw;
-				nw->parent = par;
-				return (nw);
-			}
 		}
 		else if (value > cur->n)
 		{
 			cur = cur->right;
-			if (cur == NULL)
-			{
-				par->right = nw;
-				nw->parent = par;
-				return (nw);
-			}
 		}
 		else
 		{
-			free(nw);
+			free(new);
+
 			return (NULL);
 		}
 	}
+
+	new->parent = par;
+
+	if (value < par->n)
+	{
+		par->left = new;
+	}
+	else
+	{
+		par->right = new;
+	}
+
+	return (new);
 }
